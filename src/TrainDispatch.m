6decc6c0173a8f38b05660780567099b92635baf
liuyf7 <liuyf7@shanghaitech.edu.cn>
2023-04-07 21:19:02 +0800

    时间系统底层设计

--- src/TrainDispatch.m
+++ src/TrainDispatch.m
@@ -1,12 +1,33 @@
-classdef TrainDispatch<handle
+classdef TrainDispatch < handle
     %TRAINDISPATCH 此处显示有关此类的摘要
     %   此处显示详细说明
-    
-    properties
-        activeTrains=[]
-    end
-    
-    methods
-    end
-end
+    % 该类包含了所有的实例变量，所有的数据流动逻辑都在于此
+    % 实体类的设计按照函数式编程或者容器式设计
 
+    properties
+        SysTime %该变量用于模拟火车世界时间，格式为从0900开始的以秒为单位的int型数值
+        SysTimeDisplay %该变量用于显示火车世界时间，格式为HH:MM
+        SysTimeCron
+        activeTrains = []
+    end
+
+    % ========时间模拟系统==========
+    methods
+
+        function obj = TrainDispatch()
+            "建立火车调度中心"
+            obj.SysTime = datetime('09:00:00');
+            SysTimeCron = timer('ExecutionMode', 'fixedRate', 'Period', 1, 'TimerFcn', @obj.update_sys_time,'TasksToExecute', 5);
+            start(SysTimeCron);
+        end
+
+        function update_sys_time(Obj,~, ~)
+            % "更新时间"
+            Obj.SysTime=Obj.SysTime+minutes(1);
+            Obj.SysTimeDisplay=datestr(Obj.SysTime, 'HH:MM'); % 转换为字符串格式
+
+        end
+
+    end
+
+end
--- src/main.m
+++ src/main.m
@@ -1,23 +1,20 @@
 % Entrypoint for your project.
 
-% Below is an example application created by teaching staff. You can use it as
-% a reference and remove it when you start your own project.
-
 % Initialize the UI and the controller.
 % ui = view();
 % controller = counter();
 % ui.constructor(counter);
-ui= query();
+ui= mainApp();
 % init a basic env
 trainDispath=TrainDispatch();
-shanghai=Station("Shanghai");
-hangzhou=Station("Hangzhou");
-train1=Train("G112",trainDispath);
-Xiaoming=Passanger(0,"XiaoMing");
-ui.bindPassanger(Xiaoming);
+% shanghai=Station("Shanghai");
+% hangzhou=Station("Hangzhou");
+% train1=Train("G112",trainDispath);
+% Xiaoming=Passanger(0,"XiaoMing");
+% ui.bindPassanger(Xiaoming);
 
-testTicket=Ticket(train1,shanghai,hangzhou);
-Xiaoming.bookTicket(testTicket);
-ui.DisplayTrains()
+% testTicket=Ticket(train1,shanghai,hangzhou);
+% Xiaoming.bookTicket(testTicket);
+% ui.DisplayTrains()
 
 % * YOUR CODE HERE *
