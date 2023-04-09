3f63c84a08011cac3fcf134404b74bcc3b047676
liuyf7 <liuyf7@shanghaitech.edu.cn>
2023-04-09 19:28:32 +0800

    silder启动完成

--- src/Train.m
+++ src/Train.m
@@ -17,7 +17,7 @@ classdef Train<handle
             obj.status = 'NOTSTARTED';
         end
 
-        function updateTrainStatus(timeNow)
+        function updateTrainStatus(app,timeNow)
             if app.status == 'NOTSTARTED'
                 if timeNow >= app.remainingStations(1).departureTime
                     app.trainCode+"发车!!!!"
