3e48879a3e4b8250dbe947f7cc256130c1a8ebbc
liuyf7 <liuyf7@shanghaitech.edu.cn>
2023-04-12 21:10:09 +0800

    范式完成

--- src/Train.m
+++ src/Train.m
@@ -9,20 +9,24 @@ classdef Train < handle
         status %'NOTSTARTED','RUNNING', 'STOP'
         lineType %两条线路，短的是1，长的是2
         lineDirection %从上到下是0，反过来是1
+        DorG %动车还是高铁（直达还是经停）
     end
 
     methods (Access = public)
 
-        function obj = Train(code, stations)
+        function obj = Train(code, stations,lineT)
             obj.trainCode = code;
             obj.passangers = [];
             obj.remainingStations = stations;
             obj.status = 'NOTSTARTED';
-
-            if length(stations) == 4
-                obj.lineType = 1;
+            obj.lineType = lineT;
+            %如果code以“D”开头，那么是动车，否则是高铁
+            if  ~isempty(extract(obj.trainCode,"D"))
+                "动车"
+                obj.DorG = 1;
             else
-                obj.lineType = 2;
+                "高铁"
+                obj.DorG = 2;
             end
 
         end
@@ -37,10 +41,11 @@ classdef Train < handle
                 end
 
             elseif strcmp(app.status, 'RUNNING')
+                isCrossStation=0;
                 %如果当前时间大于等于下一个站的到达时间，那么就踢掉一个站点
                 if timeNow >= app.remainingStations(2).arrivalTime
                     app.remainingStations = app.remainingStations(2:end);
-                    
+                    isCrossStation=1;
                 end
 
                 % 到达终点站的条件是：上面踢掉一个站点之后只剩下一个了
@@ -49,7 +54,9 @@ classdef Train < handle
                     app.remainingStations = [];
                     app.trainCode + "到达终点站，停车"
                 else
-                    app.trainCode + "到达站点"+app.remainingStations(2).stationName
+                    if isCrossStation==1
+                        app.trainCode + "到达站点"+app.remainingStations(2).stationName
+                    end
 
                 end
 
