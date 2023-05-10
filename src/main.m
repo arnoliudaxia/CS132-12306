43dc72bbf0bf2689278a1154a27b5396c07489f4
liuyf7 <liuyf7@shanghaitech.edu.cn>
2023-05-11 01:02:54 +0800

    依照测试集修改定义

--- src/main.m
+++ src/main.m
@@ -2,29 +2,37 @@
 
 trainDispath=TrainDispatch();
 % Initialize the UI
-mainapp= mainApp();
-mainapp.trainDispatch=trainDispath;
-mainapp.usrID="小明";
-mainapp.DropDown_SelectPassanger.Items=["就我自己","小张"];
-usr2= mainApp();
-usr2.trainDispatch=trainDispath;
-usr2.usrID="小张";
-usr2.DropDown_SelectPassanger.Items=["就我自己","小明"];
-trainDispath.usrsinfo(1).usrName="小明";
-trainDispath.usrsinfo(1).ticket=[];
-trainDispath.usrsinfo(1).usrStatus="IDLE";
-trainDispath.usrsinfo(2).usrName="小张";
-trainDispath.usrsinfo(2).ticket=[];
-trainDispath.usrsinfo(2).usrStatus="IDLE";
-trainDispath.client=[mainapp,usr2];
+usr1 = mainApp();
+usr1.trainDispatch = trainDispath;
+usr1.usrID = "XiaoShang";
+testCase.usr1 = usr1;
+trainDispath.usrsinfo(1).usrName = "XiaoShang";
+trainDispath.usrsinfo(1).ticket = [];
+trainDispath.usrsinfo(1).usrStatus = "IDLE";
+
+% usr2= mainApp();
+% usr2.trainDispatch=trainDispath;
+% usr2.usrID="小张";
+% usr2.DropDown_SelectPassanger.Items=["就我自己","小明"];
+% trainDispath.usrsinfo(2).usrName="小张";
+% trainDispath.usrsinfo(2).ticket=[];
+% trainDispath.usrsinfo(2).usrStatus="IDLE";
+
+usr1.setOtherPassengers(["XiaoShang"]);
+trainDispath.client=[usr1];
 % Initialize the debug UI
 debugUI= DebugApp();
 trainDispath.debugApp=debugUI;
-mainapp.debugApp=debugUI;
 debugUI.trainDispatch=trainDispath;
 debugUI.display_update_systime();
 debugUI.displaySeatTrain=trainDispath.Trains(2);
 % 自动化测试
-autoTest=AutoTest();
-autoTest.trainDispatch=trainDispath;
-autoTest.Test();
+% autoTest=AutoTest();
+% autoTest.trainDispatch=trainDispath;
+% autoTest.Test();
+
+testAPI=TestAPI();
+testAPI.usr1=usr1;
+testAPI.trainDispath=trainDispath;
+testAPI.findAvaTransferTicket(3,5)
+testAPI.buyTicket(1,"223",4,3)
